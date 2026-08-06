f2c052adf447ef1129de9c362f829cb7aef37cf4
Hari88-kant <harikant4880@gmail.com>
2026-08-06 08:33:49 +0530

    Add digit counting script for integers
    This script counts the number of digits in an integer, including negative values.

---
+++ no of digits in integer.js
@@ -0,0 +1,14 @@
+let num = parseInt(prompt("Enter an integer:"), 10);
+let digitCount = Math.abs(num).toString().length;
+console.log("Number of digits = " + digitCount);
+
+Sample runs:
+1. Input: 7
+   Output: Number of digits = 1
+
+2. Input: 45689
+   Output: Number of digits = 5
+
+3. Input: -1234
+   Output: Number of digits = 4
+
