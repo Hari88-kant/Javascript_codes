ad5989b469bfe5491801b9abb32ffe6758e642ab
Hari88-kant <harikant4880@gmail.com>
2026-08-06 08:31:18 +0530

    Implement palindrome checker in palindrome.js
    Add palindrome checker that prompts for input and displays result.

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+let str = prompt("Enter a string:");
+let reversed = str.split("").reverse().join("");
+if (str === reversed) {
+  console.log(str + " is a Palindrome");
+} else {
+  console.log(str + " is Not a Palindrome");
+}
+
+Sample runs:
+1. Input: madam
+   Output: madam is a Palindrome
+
+2. Input: level
+   Output: level is a Palindrome
+
+3. Input: hello
+   Output: hello is Not a Palindrome
+
