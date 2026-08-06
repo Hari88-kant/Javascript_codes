307bb07823d0833f6428769abf7e35ce30584bbb
Hari88-kant <harikant4880@gmail.com>
2026-08-06 08:27:08 +0530

    Add even or odd checker script
    This script prompts the user for a number and checks if it is even or odd, logging the result to the console.

---
+++ even or odd.js
@@ -0,0 +1,13 @@
+let num = parseInt(prompt("Enter a number:"), 10);
+if (num % 2 === 0) {
+  console.log(num + " is Even");
+} else {
+  console.log(num + " is Odd");
+}
+Sample runs:
+1. Input: 8
+   Output: 8 is Even
+2. Input: 13
+   Output: 13 is Odd
+3. Input: 0
+   Output: 0 is Even
